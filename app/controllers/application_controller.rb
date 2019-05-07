5320cac2ba76063c0c2dfb3e355eed601cb559b9
Josef Liska <josef.liska@virtualmaster.com>
2019-05-07 11:13:53 +0200

    Make simple api endpoint which shows currentlyy used db name and standby db name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  def standby_db
+    render plain: (all_db_names - [active_db_name]).first
+  end
+
+  def active_db
+    render plain: active_db_name
+  end
+
+  def active_db_name
+    Rails.configuration.database_configuration[Rails.env]['database']
+  end
+
+  def all_db_names
+    Rails.configuration.database_configuration[Rails.env]['databases'].split(', ')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'addresses/autocomplete'
   get 'addresses/:id', controller: :addresses, action: :show, as: :show_address
+  get 'standby_db', controller: :application, action: 'standby_db'
+  get 'active_db', controller: :application, action: 'active_db'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
